ebe8d333edfa03205d57b8ea6c0a3f4ac2015aa4
Otto45 <Otto45@users.noreply.github.com>
2020-07-14 21:35:44 -0400

    Added comments (#2)
    Completing PR with a squash merge

--- src/scraper-registry.ts
+++ src/scraper-registry.ts
@@ -3,6 +3,9 @@ import { TYPES } from './Dependency Injection/ioc-container';
 const scraperRegistry: { [url: string]: string } = {};
 
 scraperRegistry['https://www.zillow.com/carmel-in/'] = TYPES.zillow;
+
+// Right now only one zillow scraper can be active, until they are improved to beat the bot detection
+
 //scraperRegistry['https://www.zillow.com/westfield-in/'] = TYPES.zillow;
 
 export default scraperRegistry;
